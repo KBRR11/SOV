da83896445977b434829783031d0da1a94c703bb
Keny Ramírez <kenyramirez@upeu.edu.pe>
2019-06-16 18:58:30 -0500

    Recursos Agregados

---
+++ src/pe/com/sov/Test/Test.java
@@ -0,0 +1,25 @@
+package pe.com.sov.Test;
+
+import com.google.gson.Gson;
+
+import pe.com.sov.Dao.ProductoDao;
+import pe.com.sov.DaoImp.ProductoDaoImp;
+import pe.com.sov.Entity.Producto;
+
+public class Test {
+	private static ProductoDao pd = new ProductoDaoImp();
+	private static Gson g = new Gson();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//modificar();
+		//listar();
+		//guardarDVenta();
+		add();
+		//delete();
+	}
+	static void add() {
+		int x = pd.create(new Producto(1,"Leche Maria","123456","leche deslactosada 1lt",1.0," ",1,1));
+		System.out.println(x);
+	}
+}
